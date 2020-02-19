40c90392f5e5a6c87ae071e7b32cf02f843bdce5
Fred Bruford <fred.bruford@gmail.com>
2020-02-19 17:10:55 +0000

    full default bfd3 keymap added. _getallhitinfo works for all 10 kit parts

--- LoadGrooveFromMIDI.py
+++ LoadGrooveFromMIDI.py
@@ -25,8 +25,6 @@ def _getAllHitInfo(midi, tempo, keymap):
     # Create an array of all hits in a groove
     #Format: [quantized time index, velocity, kit piece, microtiming deviation from metronome]
 
-    # BFD KEY MAP - Incomplete
-
     hits = np.zeros([len(midi.instruments[0].notes), 4])
     i = 0
     for instrument in midi.instruments:
@@ -39,8 +37,22 @@ def _getAllHitInfo(midi, tempo, keymap):
                 hits[i, 2] = 1
             elif note.pitch in keymap["closed hihat"]:
                 hits[i, 2] = 2
+            elif note.pitch in keymap["open hihat"]:
+                hits[i,2] = 3
+            elif note.pitch in keymap["ride"]:
+                hits[i,2] = 4
+            elif note.pitch in keymap["crash"]:
+                hits[i,2] = 5
+            elif note.pitch in keymap["extra cymbal"]:
+                hits[i,2] = 6
+            elif note.pitch in keymap["low tom"]:
+                hits[i,2] = 7
+            elif note.pitch in keymap["mid tom"]:
+                hits[i,2] = 8
+            elif note.pitch in keymap["high tom"]:
+                hits[i,2] = 9
             i += 1
-    print(hits)
+
     hits[:, 0] = hits[:, 0] / 60.0 * tempo
     hits[:, 1] = hits[:, 1] / 127.0
     multipliedHit = hits[:, 0] * 4.0
@@ -53,12 +65,16 @@ def _getAllHitInfo(midi, tempo, keymap):
 
 def _setMIDIKeyMap(keymap):
     #todo: how to deal with custom keymaps?
-    # Load key map. Only deals with 10 core drum kit parts - kick, snare, closed/pedal hihat, open hihat, ride, crash,
-    # extra cymbal (any other cymbal e.g. china, splash), low tom, mid tom, high tom. All other percussion is ignored
-    # See here for a link to General MIDI key map to see exactly which instruments are mapped to which part of the
-    # groove matrix: https://musescore.org/sites/musescore.org/files/General%20MIDI%20Standard%20Percussion%20Set%20Key%20Map.pdf
+    # Set key map for MIDI transcription. Only deals with 10 core drum kit parts - kick, snare, closed/pedal hihat,
+    # open hihat, ride, crash, extra cymbal (any other cymbal e.g. china, splash), low tom, mid tom, high tom. All other
+    # percussion is ignored
+    # See here for a link to General MIDI key map to see exactly which instruments are mapped where:
+    # https://musescore.org/sites/musescore.org/files/General%20MIDI%20Standard%20Percussion%20Set%20Key%20Map.pdf
 
-    BFDKeymap = {"kick":[24], "snare":[26,29],"closed hihat":[37, 30, 42, 46],"crash":[41],"floorTom":[31]}
+    BFDKeymap = {"kick":[24,84], "snare":[25,26,27,28,29,86,88],"closed hihat":[30,32,37,42,44,46,49,51,54,58],
+                 "open hihat":[34,39,56],"ride":[50,60,71,81,87],"crash":[41,43,52,53,62,64,72,74,75,78],
+                 "extra cymbal":[45,47,48,55,57,59,65,67,69,76,77,79,80,82,85],"low tom":[31,33,61,63,89,90],
+                 "mid tom":[35,36,66,68,91,92], "high tom":[38,40,70,73,93,94]}
 
     GMKeymap = {"kick":[35,36], "snare":[37, 38, 40],"closed hihat":[42,44],"open hihat":[46],"ride":[51,53,59],
                 "crash":[49,57],"extra cymbal":[52,55], "low tom":[41,43,45], "mid tom":[47,48], "high tom":[50]}
